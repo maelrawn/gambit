5680decbb245f70ee6ee801aca8a3c179f02e80a
James DeLaura <jdelaura00@gmail.com>
2023-05-07 20:07:12 -0400

    working boardstate

--- src/main/java/graphics/ChessBoardGraphics.java
+++ src/main/java/graphics/ChessBoardGraphics.java
@@ -1,2 +1,66 @@
-package graphics;public class ChessBoard {
+package graphics;
+
+import board.ChessBoard;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class ChessBoardGraphics implements RenderableObject{
+    TextColor dark;
+    TextColor bright;
+    final public int sideLength = 8;
+    int x = 0;
+    int y = 0;
+    public ChessBoardGraphics(int x, int y, TextColor dark, TextColor bright){
+        this.dark = dark;
+        this.bright = bright;
+        this.x = x;
+        this.y = y;
+    }
+    public static int getAbsX(int x){
+        return x + ChessBoard.x;
+    }
+    public static int getAbsY(int y){
+        return y + ChessBoard.y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    @Override
+    public void draw(Screen screen) {
+        TextGraphics darkRender = screen.newTextGraphics();
+        TextGraphics brightRender = screen.newTextGraphics();
+        darkRender.setBackgroundColor(dark);
+        brightRender.setBackgroundColor(bright);
+        String topLine = "12345678";
+        String leftLine = "ABCDEFGH";
+        darkRender.putString(x, y-2, topLine);
+        for (int i = 0; i < leftLine.length(); i++ ){
+            darkRender.putString(x-2, y + i, String.valueOf(leftLine.charAt(i)));
+        }
+        for(int i = 0; i < sideLength; i++){
+            for(int j = 0; j < sideLength; j++){
+                if( (i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0) ){
+                    darkRender.putString(x+i, y+j, " ");
+                }
+                else
+                    brightRender.putString(x + i, y + j, " ");
+            }
+        }
+    }
 }
